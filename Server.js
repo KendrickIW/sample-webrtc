8302c3b38a2e690c6faca3fcbb1011a7249f3ed5
Kendrick Williams <williams.kendrick@gmail.com>
2017-09-28 11:52:37 -0400

    Fixed port number

--- Server.js
+++ Server.js
@@ -2,14 +2,15 @@ var express = require("express");
 var app     = express();
 var path    = require("path");
 
-app.use(express.static(__dirname + '/assets'));
+var port = process.env.PORT || 3000;
 
+app.use(express.static(__dirname + '/assets'));
 
 app.get('/',function(req,res){
   res.sendFile(path.join(__dirname+'/index.html'));
   //__dirname : It will resolve to your project folder.
 });
 
-app.listen(3000);
-
-console.log("Running at Port 3000");
+app.listen(port, function() {
+  console.log('Our app is running on port:', port);
+}
